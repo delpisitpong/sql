975a28dbe4f95cffc8d534a02e21e696db7350a9
delpisitpong <pisitpongchairoek@gmail.com>
2026-02-28 15:56:00 +0700

    refactor: clean up code

--- TNI_Lab/ddl.sql
+++ TNI_Lab/ddl.sql
@@ -55,42 +55,11 @@ CREATE TABLE animal_treatment (
 );
 
 
--- Adding a row to animal_treatment table
-INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
-VALUES
-    ('100', 'P001', 'Rover',  'Dog', 'Vaccinate', TO_DATE('13-Jan-23','DD-MON-RR'), '1', 500);
-
-
-INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
-VALUES
-    ('200', 'P002', 'Sport',  'Dog', 'Injure', TO_DATE('21-Jan-23','DD-MON-RR'), '2', 1000);
-
-
-INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
-VALUES
-    ('300', 'P003', 'Morris',  'Cat', 'Check Up', TO_DATE('13-Jan-23','DD-MON-RR'), '2', 200);
-
-
--- Updating the owner_id for Morris in animal_treatment table
-UPDATE animal_treatment
-SET owner_id = '1'
-WHERE pet_name = 'Morris';
-
-
 -- Creating the owner table
 CREATE TABLE owner (
  owner_id   CHAR(4)     PRIMARY KEY,
  owner_name VARCHAR(50) NOT NULL
 );
-
-
--- Adding a row to owner table
-INSERT INTO owner(owner_id, owner_name)
-VALUES('1', 'Sam');
-
-
-INSERT INTO owner(owner_id, owner_name)
-VALUES('2', 'Kim');
 
 
 -- Adding a foreign key constraint to animal_treatment table referencing owner table
@@ -110,19 +79,9 @@ NOCYCLE
 NOCACHE;
 
 
--- Inserting a new treatment record using the sequence for t_id
-INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
-VALUES(seq_treatment.NEXTVAL, 'P004', 'Tweedy', 'Dog', 'Check Up', DEFAULT, '2', DEFAULT);
-
-
 -- Creating a view to show total pay for each owner
 CREATE VIEW view_owner_pay
 AS
   SELECT owner_name, TO_CHAR(sum(charge), '999,999.00') AS "Total Pay"
   FROM owner o INNER JOIN animal_treatment a ON o.owner_id = a.owner_id
   GROUP BY owner_name;
-
-
--- Deleting records from animal_treatment table where pet_name starts with 'T' or contains 't'
-DELETE FROM animal_treatment
-WHERE pet_name LIKE 'T%' OR pet_name LIKE '%t%';
--- TNI_Lab/dml.sql
+++ TNI_Lab/dml.sql
@@ -24,3 +24,38 @@ WHERE customer_id = 'C001';
 -- Deleting a customer record
 DELETE FROM customer
 WHERE customer_id = 'C004';
+
+
+-- Adding a row to animal_treatment table
+INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
+VALUES
+    ('100', 'P001', 'Rover',  'Dog', 'Vaccinate', TO_DATE('13-Jan-23','DD-MON-RR'), '1', 500);
+
+
+INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
+VALUES
+    ('200', 'P002', 'Sport',  'Dog', 'Injure', TO_DATE('21-Jan-23','DD-MON-RR'), '2', 1000);
+
+
+INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
+VALUES
+    ('300', 'P003', 'Morris',  'Cat', 'Check Up', TO_DATE('13-Jan-23','DD-MON-RR'), '2', 200);
+
+
+-- Adding a row to owner table
+INSERT INTO owner(owner_id, owner_name)
+VALUES('1', 'Sam');
+
+
+INSERT INTO owner(owner_id, owner_name)
+VALUES('2', 'Kim');
+
+
+-- Inserting a new treatment record using the sequence for t_id
+INSERT INTO animal_treatment(t_id, pet_id, pet_name, pet_type, treatment, t_date, owner_id, charge)
+VALUES(seq_treatment.NEXTVAL, 'P004', 'Tweedy', 'Dog', 'Check Up', DEFAULT, '2', DEFAULT);
+
+
+-- Deleting records from animal_treatment table where pet_name starts with 'T' or contains 't'
+DELETE FROM animal_treatment
+WHERE pet_name LIKE 'T%' OR pet_name LIKE '%t%';
