abbfb1c6970572be683e2f8bf0595fde4156c942
delpisitpong <pisitpongchairoek@gmail.com>
2026-03-07 22:42:48 +0700

    feat: add view.sql and remove old test scripts

---
+++ TNI_Lab/view.sql
@@ -0,0 +1,12 @@
+-- language=SQL
+
+
+
+CREATE VIEW my_view
+AS SELECT course_id, assignment_name, due_date
+   FROM my_assignment
+   WHERE course_id = 'DAT-101' AND a_status = 'NOT_SUBMIT';
+   
+
+SELECT *
+FROM my_view;
